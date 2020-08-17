9879219f55f5149f52a0d553c87c76e8d7cf1001
André David <andredavid1@yahoo.com.br>
2020-08-17 08:24:03 -0300

    base_url_backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const URL_BASE_BACKEND = window.location.hostname.includes('localhost')
+    ? 'http://localhost:3333'
+    : 'https://melissa-server.herokuapp.com';
+
 const api = axios.create({
-    baseURL: 'https://melissa-server.herokuapp.com/',
+    baseURL: URL_BASE_BACKEND,
 });
 
 export default api;
